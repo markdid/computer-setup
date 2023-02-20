36487766870acf96010aca17761251fb34f5e19f
Larsens <larsens@macbook-air.lan>
2023-02-20 16:38:46 -0700

    update mac devapps

--- Mac/install-developer-tools.sh
+++ Mac/install-developer-tools.sh
@@ -1,5 +1,17 @@
 # Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install Visual Studio Code
+# brew install --cask visual-studio-code
+
+# # Install IntelliJ IDEA
+# brew install --cask intellij-idea-ce
+
+# # Install Android Studio
+# brew install --cask android-studio
+
+# Install Xcode command-line tools
+# xcode-select --install
 
 # Install Python and pip
 brew install python
@@ -8,39 +20,27 @@ brew install python
 brew install node
 
 # Install Flutter SDK
-wget https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_2.8.1-stable.zip
-unzip flutter_macos_2.8.1-stable.zip
-export PATH="$PATH:`pwd`/flutter/bin"
+# wget https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_2.8.1-stable.zip
+# unzip flutter_macos_2.8.1-stable.zip
+# export PATH="$PATH:`pwd`/flutter/bin" #temporary
+#add the following to .bash_profile such as .zshrc
+# vim ~/.zshrc
+# export PATH="$PATH:$HOME/Documents/development/flutter/bin"
 
 # Install Rust compiler and Cargo package manager
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # Install Java Development Kit (JDK)
-brew install --cask adoptopenjdk
+# brew install --cask adoptopenjdk
 
 # Install Docker Desktop
 brew install --cask docker
 
-# Install Git
-brew install git
-
-# Install Visual Studio Code
-brew install --cask visual-studio-code
-
-# Install IntelliJ IDEA
-brew install --cask intellij-idea-ce
-
-# Install Android Studio
-brew install --cask android-studio
-
-# Install Xcode command-line tools
-xcode-select --install
-
 # Install iOS Simulator
-brew install --cask ios-simulator
+# brew install --cask ios-simulator
 
 # Install Android SDK
-brew install android-sdk
+# brew install android-sdk
 
 # Install React Native CLI
 sudo npm install -g react-native-cli
